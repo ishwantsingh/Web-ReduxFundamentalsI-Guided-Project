3ac9890bd64761753614dcc898af46622012a30d
Ishwant Singh <ishwant8@gmail.com>
2019-03-15 06:36:48 +0530

    State changes using redux via different actionCreators or yells

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import { createStore, combineReducers } from 'redux';
-// import { Provider } from 'react-redux';
-import Container from './components/Container';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import Container from "./components/Container";
 
+//1- function that returns state
+const rootReducers = combineReducers({ mentalHealth, physicalHealth });
 
-// const store = createStore(
-//   combinedState,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-// );
+function mentalHealth(state = 100, action) {
+  switch (action.type) {
+    case "WATCH_THE_NEWS":
+      return state - 20;
+    case "WIN_THE_LOTTERY":
+      return state + 30;
+    default:
+      return state;
+  }
+}
+
+function physicalHealth(state = 100, action) {
+  switch (action.type) {
+    case "SLIP_ON_BANANA_PEEL":
+      return state - 20;
+    case "WIN_THE_LOTTERY":
+      return state + action.howMuch;
+    default:
+      return state;
+  }
+}
+
+//2- lets actually make that state object
+const store = createStore(
+  rootReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+//3- we need to wrap a container in a provider
 
 ReactDOM.render(
-  <Container />,
-  document.querySelector('#target'),
+  <Provider store={store}>
+    <Container />
+  </Provider>,
+  document.querySelector("#target")
 );
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,24 +1,57 @@
-import React from 'react';
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { map } from "rsvp";
 
+const slipOnBananaPeel = () => ({
+  type: "SLIP_ON_BANANA_PEEL"
+});
+const watchTheNews = () => ({
+  type: "WATCH_THE_NEWS"
+});
+const winTheLottery = howMuch => ({
+  type: "WIN_THE_LOTTERY",
+  howMuch
+});
 
-export default class Person extends React.Component {
-  state = {
-    mentalHealth: 100,
-    physicalHealth: 100,
-  }
-
+export class Person extends React.Component {
   render() {
     return (
       <div>
-        <div>physical health: {this.state.physicalHealth}</div>
-        <div>mental health: {this.state.mentalHealth}</div>
-
-        <button>Slip on banana peel</button>
-        <button>Watch the news</button>
-        <button>Win the Lottery</button>
+        <div>physical health: {this.props.physicalHealth}</div>
+        <div>mental health: {this.props.mentalHealth}</div>
+        {/* children would get slices of state and methods to change state */}
+        <button onClick={this.props.slipOnBananaPeel}>
+          Slip on banana peel
+        </button>
+        <button onClick={this.props.watchTheNews}>Watch the news</button>
+        <button onClick={() => this.props.winTheLottery(15)}>
+          Win the Lottery
+        </button>
       </div>
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      slipOnBananaPeel,
+      watchTheNews,
+      winTheLottery
+    },
+    dispatch
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    physicalHealth: state.physicalHealth,
+    mentalHealth: state.mentalHealth
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Person);
